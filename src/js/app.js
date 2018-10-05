1e089b8163b72a8a31c62047cfce9abd068d7932
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2018-10-05 20:06:59 +0200

    added frame and animation to box with mobile app

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,27 @@
 import header from "./header";
 import scroll from "./scroll";
+// import gif from "./showgif";
 
 
 
 $(function() {
 
     header();
-        scroll();
+    scroll();
+    // gif();
+
+    const overlay = document.querySelector('.project-box-image-overlay');
+    const $frame= $(".project-box-image")
+    const box = document.querySelector('.project-box-image-overlay');
+    let img = document.querySelector(".project-box-image img");
+    // console.log(img);
+
+     overlay.addEventListener("click", function(){
+
+   $(this).toggleClass("hidden");
+
+   // $img.attr('src','https://github.com/dagmarawieczorek/FoodPlannerApp/blob/master/images/gif-test-4.gif');
+
+    })
 
 });
--- src/js/showgif.js
+++ src/js/showgif.js
@@ -1,8 +1,5 @@
 function gif() {
-    const box = document.querySelector('.project-box-image-overlay');
-console.log(box)
-  // box.addEventListener("click", function(){
-  // console.log("klikk")
-  // })
+
+
 }
 export default gif();
