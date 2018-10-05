3ff259f5e06d4cf134403c37f00701c241a2db92
dagmarawieczorek <dagmara.sylwia.w@gmail.com>
2018-10-05 19:42:32 +0200

    added frame and animation to box with mobile app

---
+++ src/js/showgif.js
@@ -0,0 +1,8 @@
+function gif() {
+    const box = document.querySelector('.project-box-image-overlay');
+console.log(box)
+  // box.addEventListener("click", function(){
+  // console.log("klikk")
+  // })
+}
+export default gif();
